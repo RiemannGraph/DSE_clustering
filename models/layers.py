19d542ede4246dc136b24fa77550c5dc3b03a246
ZhenhHuang <huangzhenhao_work@163.com>
2024-12-09 13:07:43 +0800

    Update layers.py

--- models/layers.py
+++ models/layers.py
@@ -197,21 +197,21 @@ class LorentzTransformation(nn.Module):
     """
     Input size: [N, in_dim]
     v : [1, in_dim]
-    W : [out_dim, in_dim]
-    Output size: [N, out_dim + 1]
+    W : [out_dim - 1, in_dim]
+    Output size: [N, out_dim]
     """
     def __init__(self, in_dim, out_dim):
         super(LorentzTransformation, self).__init__()
         self.v = nn.Parameter(torch.randn(1, in_dim), requires_grad=True)
-        if out_dim > in_dim:
-            self.theta = nn.Parameter(torch.randn(out_dim-in_dim, in_dim), requires_grad=True)
+        if (out_dim - 1) > in_dim:
+            self.theta = nn.Parameter(torch.randn(out_dim - 1 - in_dim, in_dim), requires_grad=True)
         else:
             self.theta = None
         diag = torch.ones(in_dim)
         diag.narrow(-1, 0, 1).mul_(-1)
         self.metric = nn.Parameter(torch.diag(diag), requires_grad=False)
         self.in_dim = in_dim
-        self.out_dim = out_dim
+        self.out_dim = out_dim - 1
 
     def forward(self, x):
         vvT = self.v.t() @ self.v + self.metric
@@ -219,9 +219,10 @@ class LorentzTransformation(nn.Module):
         if self.theta is not None:
             U = torch.concat([U, self.theta], dim=0)
             L = torch.diag(eig.clamp(min=0.).sqrt())
+            W = U @ L
         else:
             U = U.narrow(1, self.in_dim - self.out_dim, self.out_dim)
             L = torch.diag(eig.narrow(0, self.in_dim - self.out_dim, self.out_dim).clamp(min=0.).sqrt())
-        W = U @ L
-        x = torch.concat([x @ self.v.t(), x @ W], dim=-1)
+            W = L @ U.t()
+        x = torch.concat([x @ self.v.t(), x @ W.t()], dim=-1)
         return x
